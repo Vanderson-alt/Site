dbb9cdb9c325e80589579680c3f09ea556ac9144
Vanderson <jordyabc2004@gmail.com>
2026-05-01 19:45:48 -0300

    Atualizado

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
-function teste() {
-  alert("Manutenção");
+const carrosel = document.querySelector(".imagens-container");
+
+let larguraOriginal = carrosel.scrollWidth;
+
+carrosel.innerHTML += carrosel.innerHTML;
+carrosel.innerHTML += carrosel.innerHTML;
+
+let pos = 0;
+
+function slider() {
+  pos += 1;
+
+  if (pos >= larguraOriginal) {
+    carrosel.style.transition = "none";
+    pos = 0;
+    carrosel.style.transform = `translateX(0px)`;
+
+    setTimeout(() => {
+      carrosel.style.transition = "transform 0.016s linear";
+    }, 10);
+  } else {
+    carrosel.style.transition = "transform 0.016s linear";
+    carrosel.style.transform = `translateX(-${pos}px)`;
+  }
 }
+
+setInterval(slider, 16);
+
+window.addEventListener("resize", () => {
+  larguraOriginal = carrosel.scrollWidth / 4;
+  pos = 0;
+  carrosel.style.transition = "none";
+  carrosel.style.transform = `translateX(0px)`;
+});
